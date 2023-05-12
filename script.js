a46a71d125e06479e983153762c83fa009aa8579
ryanjnam99 <ryanjnam@yahoo.com>
2023-05-11 21:45:01 -0700

    really finished

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-j
+for (var i=1; i<21; i++) {
+    if (i%2 !== 0) {
+        console.log(i);
+    }
+}
+
+for (var i=100; i>=0; i--) {
+    if (i%3 == 0 && i%2 == 0) {
+        console.log(i);
+    }
+}
+
+for (var i=4.5; i>=-3.5; i--) {
+    i-=0.5;
+    console.log(i);
+}
+
+var sum = 0;
+for (var i=1; i<101; i++) {
+    sum += i;
+}
+console.log(sum);
+
+var product = 1;
+for (var i=1; i<13; i++) {
+    product*=i;
+}
+console.log(product)
+
+
